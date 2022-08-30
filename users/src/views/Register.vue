0ced608b28414146bca5e9215a1e356000ab8c3a
brunobandelli <brunobandelli@gmail.com>
2022-08-30 20:19:11 -0300

    home register

---
+++ users/src/views/Register.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <h2>Registro de usuários</h2>
+    <hr>
+    <div class="columns is-centered">
+  <div class="column is-half">    
+        <p>Nome</p>
+        <input type="text" placeholder="Nome do usuário" class="input" v-model="name">
+        <p>E-mail</p>
+        <input type="email" placeholder="email@email.com" class="input" v-model="email">
+        <p>Senha</p>
+        <input type="password" placeholder="******" class="input" v-model="password">
+        <hr>
+        <button class="button is-success" @click="register" >Cadastrar</button>
+  </div>
+</div>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+    data(){
+        return{
+            name: '',
+            password: '',
+            email: ''
+        }
+    },
+    methods: {
+        register(){
+            axios.post("http://localhost:8686/user",{
+                name: this.name,
+                password: this.password,
+                email: this.email
+            }).then(res=>{
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err.response);
+            })
+            // console.log(this.name),
+            // console.log(this.password),
+            // console.log(this.email)
+        }
+    }
+
+}
+ 
+</script>
+
+<style scoped>
+
+</style>
